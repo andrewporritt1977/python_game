e858fa3d79cab22c534a88f06b466396330466ea
Andrew Porritt <aporritt@and.digital>
2020-03-10 10:42:27 +0000

    game loop working.

--- app.py
+++ app.py
@@ -11,12 +11,32 @@ target = random.randint(1,10)
 player_name = input("Please enter your name - ")
 round = 3
 
-guess = int(input("Thanks " + player_name + " Guess my number please?"))
-if guess == target :
-    print('\nYep')
-elif guess < target:
-    print('\nHigher')
-elif guess > target:
-    print('\nLower')
-else:
-    print('\Try again')
+i = 0
+while i < round :
+    guess = int(input("Thanks " + player_name + " Guess my number please?"))
+    if guess == target :
+        print('\nYep')
+        break
+    elif guess < target and i < round:
+        print('\nHigher')
+        i += 1
+        continue
+    elif guess > target and i < round:
+        print('\nLower')
+        i += 1
+        continue
+    elif i == round :
+        print('\nYa done')
+        break
+
+
+
+# guess = int(input("Thanks " + player_name + " Guess my number please?"))
+# if guess == target :
+#     print('\nYep')
+# elif guess < target:
+#     print('\nHigher')
+# elif guess > target:
+#     print('\nLower')
+# else:
+#     print('\Try again')
