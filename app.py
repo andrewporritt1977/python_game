a2bfe57748614861c40c447d75c8101cfa675a15
Andrew Porritt <aporritt@and.digital>
2020-03-10 11:18:02 +0000

    working loop

--- app.py
+++ app.py
@@ -7,30 +7,40 @@ print ("**  Number Guess  **")
 print ("********************")
 time.sleep(2)
 os.system('clear')
-target = random.randint(1,10)
+target = 4
+
+#random.randint(1,10)
+
 player_name = input("Please enter your name - ")
 round = 3
 score = 0
 
-i = 0
-while i < round :
-    guess = int(input("Thanks " + player_name + " Guess my number please?"))
-    if guess == target :
-        print('\nYou have been successful')
-        score+=1
-        print('your score is ' + str(score) + " " + player_name)
-        break
-    elif guess < target and i < round:
-        print('\nHigher')
-        i += 1
-        continue
-    elif guess > target and i < round:
-        print('\nLower')
-        i += 1
-        continue
-    elif i == round :
-        print('\nYa done')
-        break
+play_round = input('Would you like to play a round?')
+
+while play_round == 'yes':
+
+    i = 0
+    while i < round :
+        guess = int(input("Thanks " + player_name + " Guess my number please?"))
+        if guess == target :
+            print('\nYou have been successful')
+            score += 1
+            print('your score is ' + str(score) + " " + player_name)
+            round = 3
+ 
+        elif guess < target:
+            print('\nHigher')
+            i += 1
+            continue
+        elif guess > target:
+            print('\nLower')
+            i += 1
+            continue
+    
+    print('\nYa done')
+    play_round = 'no'
+
+
 
 
 
