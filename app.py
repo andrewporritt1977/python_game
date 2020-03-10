df968dc5ee3d42ef3cff95de90417bbdc1d8eef9
Andrew Porritt <aporritt@and.digital>
2020-03-10 10:02:01 +0000

    added basic game win logic

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-print ("Sup World!")
+import os
+import time
+import random
+
+print ("********************")
+print ("**  Number Guess  **")
+print ("********************")
+time.sleep(2)
+os.system('clear')
+target = random.randint(1,10)
+guess = int(input("Guess my number please?"))
+
+if guess == target :
+    print('\nYep')
+elif guess < target:
+    print('\nHigher')
+elif guess > target:
+    print('\nLower')
+else:
+    print('\Try again')
