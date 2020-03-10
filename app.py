920782157b9b9d948c6b72581bd711870f64e47c
Andrew Porritt <aporritt@and.digital>
2020-03-10 13:38:27 +0000

    fixed loop

--- app.py
+++ app.py
@@ -12,40 +12,47 @@ class Main:
     print ("**  Number Guess  **")
     print ("********************")
     time.sleep(2)
-    os.system('clear')
-    target = random.randint(1,10)
 
-    player = Player(input("Please enter your name - "))
-    round = 3
+    def game():
+        os.system('clear')
+        target = random.randint(1,10)
+        player = Player(input("Please enter your name - "))
+        round = 3
 
-    play_round = input('Would you like to play a round? (y/n)')
+        play_round = input('Would you like to play a round? (y/n)')
 
-    while play_round == 'y':
+        while play_round == 'y':
 
-        i = 0
-        while i < round :
-            guess = int(input("Thanks " + player.name + " Guess my number please?\nIt's between 1 and 10 - "))
-            if guess == target :
-                print('\nYou have been successful')
-                player.addToScore(1)
-                print('your score is ' + str(player.score) + " " + player.name)
-                i = 0
-                target = random.randint(1,10)
-            elif guess < target:
-                print('\nHigher')
-                i += 1
-                continue
-            elif guess > target:
-                print('\nLower')
-                i += 1
-                continue
-        
-        print('\nYa done')
-        play_round = 'n'
+            i = 0
+            while i < round :
+                guess = int(input("Thanks " + player.name + " Guess my number please?\nIt's between 1 and 10 - "))
+                if guess == target :
+                    print('\nYou have been successful')
+                    player.addToScore(1)
+                    print('your score is ' + str(player.score) + " " + player.name)
+                    i = 0
+                    target = random.randint(1,10)
+                elif guess < target:
+                    print('\nHigher')
+                    i += 1
+                    continue
+                elif guess > target:
+                    print('\nLower')
+                    i += 1
+                    continue
+            
+            print('\nYa done')
+            play_round = 'n'
 
-    leaderboard = Leaderboard()
-    leaderboard.addPlayerToList(player)
-    leaderboard.printLeaderBoard()
+        leaderboard = Leaderboard()
+        leaderboard.addPlayerToList(player)
+        leaderboard.printLeaderBoard()
+        play_again = input('Another player (y/n)')
+        if play_again == 'y':
+            time.sleep(1)
+            Main.game()
+Main.game()
+
 
 
 
